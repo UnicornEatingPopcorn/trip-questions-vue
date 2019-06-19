dbd0d1830b29886bee2d3eeb8e80c02ceec10add
Anna Evlanova <malory6072@gmail.com>
2019-06-19 15:40:45 +0300

    add Monseratt font

--- src/App.vue
+++ src/App.vue
@@ -19,6 +19,8 @@ export default {
 </script>
 
 <style lang="sass">
+@import url('https://fonts.googleapis.com/css?family=Montserrat&display=swap')
+
 #app
   width: 100%
   height: 80%
@@ -52,4 +54,7 @@ h1
 
 .top-margin
   margin-top: 40px
+
+body
+  font-family: 'Montserrat', sans-serif
 </style>
